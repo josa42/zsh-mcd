6d1639db135886caed2f03f1493e25526031463a
Josa Gesell <josa@gesell.me>
2019-08-26 11:05:22 +0200

    Add ccd

---
+++ README.md
@@ -0,0 +1,6 @@
+# Change Dir Helper Fucntions
+
+- `mcd` (`mkdir -p` and `cd` )
+- `ccd` (`hub clone` and `cd` )
+
+
--- mcd.zsh
+++ mcd.zsh
@@ -7,3 +7,14 @@ mcd() {
 _mcd() {
   _files -W "$1" -/
 }
+
+ccd() {
+  dir="$(echo $1 | xargs -n1 basename | sed 's/\.git$//')"
+
+  if ! test -d "$dir"; then
+    hub clone "$1" "$dir"
+  fi
+
+  cd "$dir" || return 1
+}
+
